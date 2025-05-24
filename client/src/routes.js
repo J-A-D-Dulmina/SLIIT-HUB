3efe44308a8a1d7cdb635588bbb9ca0e104eb161
J-A-D-Dulmina <jaddulmina@gmail.com>
2025-05-25 01:44:10 +0530

    Register Page created

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ForgotPasswordPage from './modules/user/pages/ForgotPasswordPage.jsx';
 import LoginPage from './modules/user/pages/LoginPage.jsx';
+import RegisterPage from './modules/user/RegisterPage.jsx'; 
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<LoginPage />} /> 
       <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-      <Route path="/" element={<LoginPage />} /> {/* This makes / go to login */}
-      {/* Add more routes here as you create more pages */}
+      <Route path="/register" element={<RegisterPage />} />
+      
+      
     </Routes>
   );
 };
